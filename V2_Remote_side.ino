876518c56f5c723a4d40658142e842fdda8745ea
Varga Levente István <47919682+Varga-Levente@users.noreply.github.com>
2025-11-16 17:30:53 +0100

    Update V2_Remote_side.ino

--- V2_Remote_side.ino
+++ V2_Remote_side.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include "CRC16.h"
 
 #define LORA_SCK 18
 #define LORA_MISO 19
@@ -9,54 +10,52 @@
 #define LORA_DIO0 2
 #define LORA_BAND 433E6
 
-#define ROBOT_ID 0x69
-#define SECRET_KEY 0b11011010
+#define ROBOT_ID 1
 
-#define BTN_LEFT_FWD     25
-#define BTN_LEFT_BACK    26
-#define BTN_RIGHT_FWD    32
-#define BTN_RIGHT_BACK   33
-#define BTN_SPEED        27
-#define BTN_UNUSED       13
-
-byte calcChecksum(byte id, byte cmd) {
-  return (id + cmd) & 0xFF;        // ✔ KÖNNYEN ÁTÍRHATÓ
-}
+#define BTN_LF  32
+#define BTN_LB  33
+#define BTN_RF  25
+#define BTN_RB  26
+#define BTN_SPEED 27
 
 void setup() {
+  pinMode(BTN_LF, INPUT_PULLUP);
+  pinMode(BTN_LB, INPUT_PULLUP);
+  pinMode(BTN_RF, INPUT_PULLUP);
+  pinMode(BTN_RB, INPUT_PULLUP);
+  pinMode(BTN_SPEED, INPUT_PULLUP);
+
   Serial.begin(115200);
-
-  pinMode(BTN_LEFT_FWD,   INPUT_PULLUP);
-  pinMode(BTN_LEFT_BACK,  INPUT_PULLUP);
-  pinMode(BTN_RIGHT_FWD,  INPUT_PULLUP);
-  pinMode(BTN_RIGHT_BACK, INPUT_PULLUP);
-  pinMode(BTN_SPEED,      INPUT_PULLUP);
-  pinMode(BTN_UNUSED,     INPUT_PULLUP);
-
-  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
   LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
 
   if (!LoRa.begin(LORA_BAND)) {
-    Serial.println("LoRa init ERROR!");
+    Serial.println("LoRa failed!");
     while (1);
   }
+  Serial.println("TX Ready");
 }
 
 void loop() {
   byte cmd = 0;
 
-  if (digitalRead(BTN_LEFT_FWD)   == LOW) cmd |= 0b00000001;
-  if (digitalRead(BTN_LEFT_BACK)  == LOW) cmd |= 0b00000010;
-  if (digitalRead(BTN_RIGHT_FWD)  == LOW) cmd |= 0b00000100;
-  if (digitalRead(BTN_RIGHT_BACK) == LOW) cmd |= 0b00001000;
-  if (digitalRead(BTN_SPEED)      == LOW) cmd |= 0b00010000;
+  if (!digitalRead(BTN_LF)) cmd |= 0b00000001;
+  if (!digitalRead(BTN_LB)) cmd |= 0b00000010;
+  if (!digitalRead(BTN_RF)) cmd |= 0b00000100;
+  if (!digitalRead(BTN_RB)) cmd |= 0b00001000;
 
-  byte checksum = calcChecksum(ROBOT_ID, cmd);
+  byte speedBtn = (!digitalRead(BTN_SPEED) ? 1 : 0);
+
+  byte packet[3];
+  packet[0] = ROBOT_ID;
+  packet[1] = cmd;
+  packet[2] = speedBtn;
+
+  uint16_t crc = CRC16.ccitt(packet, 3);
 
   LoRa.beginPacket();
-  LoRa.write(ROBOT_ID);
-  LoRa.write(cmd ^ SECRET_KEY);
-  LoRa.write(checksum ^ SECRET_KEY);
+  LoRa.write(packet, 3);
+  LoRa.write((crc >> 8) & 0xFF);
+  LoRa.write(crc & 0xFF);
   LoRa.endPacket();
 
   delay(50);
